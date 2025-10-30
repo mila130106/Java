082aec88b456ebc3a744bd1bdd6c2c11417b91e8
mila130106 <yeremichukk@gmail.com>
2025-10-30 22:02:57 +0200

    Create DataSerializationException.java

---
+++ lab_10/src/main/java/serialization/DataSerializationException.java
@@ -0,0 +1,7 @@
+package serialization;
+
+public class DataSerializationException extends RuntimeException {
+    public DataSerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
